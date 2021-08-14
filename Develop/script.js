3791a157137d1df6a952b955eac64a038f17351d
Lydia Williamson <lydia.williamson.OSU@gmail.com>
2021-08-13 22:48:33 -0400

    so close! I just need to figure out why my if statements in getPrompts will not all appear... it seems like only the lowercase or the first if statement is read

--- Develop/script.js
+++ Develop/script.js
@@ -7,67 +7,66 @@ var choiceArray = [];
 
 var lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var specialCharacter =   ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "<", "=", ">", "?", "@", "[", "]", "^", "_", "{", "|", "}", "~"];
+var specialCharacter = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "<", "=", ">", "?", "@", "[", "]", "^", "_", "{", "|", "}", "~"];
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// Write password to the #password input
+function writePassword() {
+
+  // starts by asking all the prompts after the generate button is clicked 
+ getPrompts ();
+
+  var Password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = Password;
+}
 
 function getPrompts () {
   // reseting choice array to always start fresh
   choiceArray = [];
 
   // asking the user the length of their desired password while using parseInt so that the string will be converted to an integer
-  characterLength = parseInt(prompt("How many charcters would you like your password to be (between 8-128)?"));
+  characterLength = parseInt(prompt("How many characters would you like your password to be (between 8-128)?"));
 
   // setting perameters so that user picks a number between 8 and 128, if they do not then an alert will show asking them to try again. 
-  if (isNaN(characterLength) && characterLength > 8 && characterLength < 128) {
-    alert("Please list a number between 8 and 128. Try again!!")
+  if (isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
+    alert("Please list a NUMBER between 8 and 128.");
     return false; 
   }
 
-  // asking user if they would like lowercase letters in their password and if so then we will add the array listed above
-  if (confirm("Would you like to include lowercase letters in your password?")) {
+  // asking user if they would like lowercase letters in their password and if so then we will add the lowercase letters to the blank array to reference in a later function
+  if(confirm("Would you like to include lowercase letters in your password?")) {
     choiceArray = choiceArray.concat(lowerLetters);
   }
 
-  // asking user if they would like uppercase letters in their password and if yes we will add the array listed above
-  if (confirm("Would you like to include lowercase letters in your password?")) {
+  // asking user if they would like uppercase letters in their password and if yes we will add the uppercase letters to the blank array to reference in a later function
+  if(confirm("Would you like to include lowercase letters in your password?")) {
     choiceArray = choiceArray.concat(upperLetters);
   }
 
-  // asking user if they would like numbers in their password and if yes we will add the array listed above
-  if (confirm("Would you like to include numbers in your password?")) {
+  // asking user if they would like numbers in their password and if yes we will add the numbers to the blank array to reference in a later function
+  if(confirm("Would you like to include numbers in your password?")) {
     choiceArray = choiceArray.concat(numbers);
   }
 
-  // asking user if they would like special characters in their password and if yes we will add the array listed above
-  if (confirm("Would you like to include special characters in your password?")) {
+  // asking user if they would like special characters in their password and if yes we will add the special char to the blank array to reference in a later function
+  if(confirm("Would you like to include special characters in your password?")) {
     choiceArray = choiceArray.concat(specialCharacter);
   }
   return true;
 }
 
 
-// Write password to the #password input
-function writePassword() {
-
-  // starts by asking all the prompts after the generate button is clicked 
-  getPrompts ();
-
-  var newPassword = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = newPassword;
-}
-
 // generating the password based on that the answers to the prompts 
 function generatePassword () {
-  var Password = "";
+  var setUpRandom = "";
 
   // creating a for loop in order to "randomly generate" the characters
   for(var i = 0; i < characterLength; i++) {
-
     var randomNumber = Math.floor(Math.random() * characterLength);
-    Password += Password + choiceArray[randomNumber+1];
+    setUpRandom = setUpRandom + choiceArray[randomNumber];
   }
-  return password;
+  return setUpRandom;
 }
 
 // Add event listener to generate button so that when the generate button is clicked then the write password function is set off
