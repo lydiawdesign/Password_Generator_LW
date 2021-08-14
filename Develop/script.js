4d8c4761e341900e884f83e5884604c8f18bf78a
Lydia Williamson <lydia.williamson.OSU@gmail.com>
2021-08-13 22:09:50 -0400

    updated the functions- still need to fix the for loop in the generatePassword fucntion. Right now I can't get the Math.floor to work properly. Stay tuned!

--- Develop/script.js
+++ Develop/script.js
@@ -11,11 +11,14 @@ var specialCharacter =   ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",",
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 function getPrompts () {
+  // reseting choice array to always start fresh
+  choiceArray = [];
+
   // asking the user the length of their desired password while using parseInt so that the string will be converted to an integer
   characterLength = parseInt(prompt("How many charcters would you like your password to be (between 8-128)?"));
 
   // setting perameters so that user picks a number between 8 and 128, if they do not then an alert will show asking them to try again. 
-  if (isNaN(characterLength) || characterLength < 8 || characterLength < 128) {
+  if (isNaN(characterLength) && characterLength > 8 && characterLength < 128) {
     alert("Please list a number between 8 and 128. Try again!!")
     return false; 
   }
@@ -24,7 +27,6 @@ function getPrompts () {
   if (confirm("Would you like to include lowercase letters in your password?")) {
     choiceArray = choiceArray.concat(lowerLetters);
   }
-
 
   // asking user if they would like uppercase letters in their password and if yes we will add the array listed above
   if (confirm("Would you like to include lowercase letters in your password?")) {
@@ -37,28 +39,38 @@ function getPrompts () {
   }
 
   // asking user if they would like special characters in their password and if yes we will add the array listed above
-  if (confirm("Would you like to include numbers in your password?")) {
+  if (confirm("Would you like to include special characters in your password?")) {
     choiceArray = choiceArray.concat(specialCharacter);
   }
+  return true;
 }
 
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+
+  // starts by asking all the prompts after the generate button is clicked 
+  getPrompts ();
+
+  var newPassword = generatePassword();
   var passwordText = document.querySelector("#password");
+  passwordText.value = newPassword;
+}
 
-  passwordText.value = password;
+// generating the password based on that the answers to the prompts 
+function generatePassword () {
+  var Password = "";
 
+  // creating a for loop in order to "randomly generate" the characters
+  for(var i = 0; i < characterLength; i++) {
+
+    var randomNumber = Math.floor(Math.random() * characterLength);
+    Password += Password + choiceArray[randomNumber+1];
+  }
+  return password;
 }
 
 // Add event listener to generate button so that when the generate button is clicked then the write password function is set off
 generateBtn.addEventListener("click", writePassword);
-
-// generating the password based on that the answers to the prompts 
-function generatePassword () {
-
-}
-
 
 
